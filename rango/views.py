258e7e0254253a549cb3f2ab74d6394fb55cc30d
fan yang <2359448y@student.gla.ac.uk>
2019-02-11 23:59:14 +0000

    chapter10

--- rango/views.py
+++ rango/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
 
 
 def index(request):
@@ -15,8 +16,11 @@ def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
     context_dict = {'categories': category_list, 'pages': page_list}
-
-    return render(request, 'rango/index.html', context=context_dict)
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+    response = render(request, 'rango/index.html', context=context_dict)
+    return response
+    # return render(request, 'rango/index.html', context=context_dict)
 
 
 def about(request):
@@ -25,8 +29,13 @@ def about(request):
     if request.session.test_cookie_worked():
         print('TEST COOKIE WORKED')
         request.session.delete_test_cookie()
+    context_dict = {}
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+    response = render(request, 'rango/about.html', context=context_dict)
+    return response
 
-    return render(request, 'rango/about.html', {})
+    # return render(request, 'rango/about.html', {})
 
 
 def show_category(request, category_name_slug):
@@ -159,3 +168,24 @@ def user_logout(request):
     logout(request)
     return HttpResponseRedirect(reverse('index'))
 
+
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+    # 如果距上次访问已超过一天……
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        # 增加访问次数后更新“last_visit”cookie
+        request.session['last_visit'] = str(datetime.now())
+    else:  # 设定“last_visit”cookie
+        request.session['last_visit'] = last_visit_cookie
+# 更新或设定“visits”cookie
+    request.session['visits'] = visits
