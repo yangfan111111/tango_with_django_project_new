e6a235948ce5b030073fee94bb11d583c30b6821
fan yang <2359448y@student.gla.ac.uk>
2019-02-11 23:11:11 +0000

    chapter9

--- rango/views.py
+++ rango/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from rango.models import Category
 from rango.models import Page
 from rango.forms import CategoryForm, PageForm
+from rango.forms import UserForm, UserProfileForm
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
     # context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    request.session.set_test_cookie()
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
     context_dict = {'categories': category_list, 'pages': page_list}
@@ -16,6 +21,11 @@ def index(request):
 
 def about(request):
     # return HttpResponse("Rango says here is the about page.<br/><a href='/rango/'>Index</a>")
+    request.session.set_test_cookie()
+    if request.session.test_cookie_worked():
+        print('TEST COOKIE WORKED')
+        request.session.delete_test_cookie()
+
     return render(request, 'rango/about.html', {})
 
 
@@ -71,4 +81,81 @@ def add_page(request, category_name_slug):
     context_dict = {'form': form, 'category': category}
 
     return render(request, 'rango/add_page.html', context_dict)
+
+
+def register(request):
+    registered = False
+
+    if request.method == 'POST':
+        user_form = UserForm(data=request.POST)
+        profile_form = UserProfileForm(data=request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+
+            user.set_password(user.password)
+            user.save()
+
+            profile = profile_form.save(commit=False)
+            profile.user = user
+
+            if 'picture' in request.FILES:
+                profile.picture = request.FILES['picture']
+
+            profile.save()
+
+            registered = True
+
+        else:
+            print(user_form.errors, profile_form.errors)
+
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+
+    return render(request, 'rango/register.html',
+                  {'user_form': user_form,
+                   'profile_form': profile_form,
+                   'registered': registered})
+
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+            else:
+
+                return render(request, "rango/restricted.html", {"message": "Your Rango account is disabled."})
+        else:
+            print("Invalid login details: {0}, {1}".format(username, password))
+            return render(request, "rango/restricted.html", {"message": "Invalid login details: {0}, {1}"
+                          .format(username, password)})
+
+    else:
+        return render(request, 'rango/login.html', {})
+
+
+def some_view(request):
+    if not request.user.is_authenticated():
+        return HttpResponse("You are logged in.")
+    else:
+        return HttpResponse("You are not logged in.")
+
+
+@login_required
+def restricted(request):
+    return HttpResponse("Since you're logged in, you can see this text!")
+
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
 
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -40,6 +40,13 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    'django.contrib.auth.hashers.BCryptPasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+]
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -109,6 +116,10 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': {'min_length': 6, }
+    },
 ]
 
 
@@ -131,3 +142,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
+LOGIN_URL = '/rango/login/'
+
