8ca04ac10ea5d876d56c8f43803e7bcc286218ab
fan yang <2359448y@student.gla.ac.uk>
2019-02-12 16:44:57 +0000

    fix error

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -13,15 +13,11 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-
-
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
-STATICFILES_DIRS = [STATIC_DIR, ]
-MEDIA_ROOT = MEDIA_DIR
+
 
 print(__file__)
 print(os.path.dirname(__file__))
@@ -143,4 +139,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 LOGIN_URL = '/rango/login/'
-
+STATICFILES_DIRS = [STATIC_DIR, ]
+MEDIA_ROOT = MEDIA_DIR
